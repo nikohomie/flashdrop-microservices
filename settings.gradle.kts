9645be9772a99f9cb905f049b6b685fb15805701
nikohomie <proyectoapp2025@gmail.com>
2026-07-21 16:00:48 -0400

    chore: eliminar modulo catalog-service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "flashdrop-microservices"
 include("shared-observability")
 include("auth-service")
 // Próximos módulos de la migración:
-include("catalog-service")
+// include("catalog-service")
 // include("orders-service")
 // include("delivery-service")
 // include("api-gateway")
